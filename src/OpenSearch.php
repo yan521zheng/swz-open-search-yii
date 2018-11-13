80f8f34b8788aa95f32b83fa5e90f8fecaeb9178
swz <yan521zheng@163.com>
2018-11-13 10:19:54 +0800

    修改优化

--- src/OpenSearch.php
+++ src/OpenSearch.php
@@ -5,20 +5,42 @@
  * Date: 2018/11/10
  * Time: 13:46
  */
+
 namespace swz\opensearch;
+require_once("OpenSearch/Autoloader/Autoloader.php");
 use OpenSearch\Client\OpenSearchClient;
+use OpenSearch\Client\SearchClient;
+use OpenSearch\Generated\Search\SearchParams;
+use OpenSearch\Util\SearchParamsBuilder;
 use yii\base\Component;
+use yii\web\BadRequestHttpException;
 
-class OpenSearch extends  Component
+/**
+ * Class OpenSearch
+ * @package swz\opensearch
+ * @property SearchClient searchClient
+ * @property SearchParamsBuilder searchParams
+ */
+class OpenSearch extends Component
 {
-    public $accessKeyId;
-    public $secret;
-    public $endPoint;
+    //设置config子句的start值
+    public $searchStart = 0;
+    //设置config子句的hit值
+    public $hit = 20;
+    //指定应用用于搜索
     public $appName;
-    public $suggestName;
-    public $options;
+    //搜索索引
+    public $searchIndex = 'default';
+    //搜索关键词
+    public $keyWords;
+    //指定返回数据格式
+    public $format = 'fulljson';
+    //排序字段
+    public $sorts;
 
-    public $_client;
+    public $_searchClient;
+
+    public $_searchParams;
 
     /**
      * @author: SWZ
@@ -29,34 +51,26 @@ class OpenSearch extends  Component
     public function init()
     {
         parent::init();
-        if (!$this->accessKeyId) {
-            throw new Exception('accessKeyId is empty');
-        }
-        if (!$this->secret) {
-            throw new Exception('secret is empty');
-        }
-
-        if (!$this->endPoint) {
-            throw new Exception('endPoint is empty');
+        if (!$this->appName) {
+            throw new \Exception('appName is empty');
         }
     }
 
     /**
-     * 获取OpenSearchClient实例
-     * @return OpenSearchClient
+     * 获取SearchClient
+     * @return SearchClient
      * @author: SWZ
-     * @time: 17:09
-     * @date: 2018/11/10
+     * @time: 9:26
+     * @date: 2018/11/13
      * @describe:
      */
-    public function getClient()
+    public function getSearchClient()
     {
-        if ($this->_client === null) {
-            $this->_client  = new OpenSearchClient($accessKeyId, $secret, $endPoint, $options);
+        if ($this->_searchClient === null) {
+            $this->_searchClient=  new SearchClient(\Yii::$app->openSearch->client);
         }
-        return $this->_client;
+        return $this->_searchClient;
     }
-
     /**
      * 搜索
      * @param array $params
@@ -66,9 +80,53 @@ class OpenSearch extends  Component
      * @date: 2018/11/10
      * @describe:
      */
-    public function search($params = [])
+    public function search()
     {
-        $search = new CloudsearchSearch($this->client);
-        return $search->search($params);
+        $searchClient = $this->searchClient;
+        // 执行搜索，获取搜索结果
+        $ret = $searchClient->execute($this->searchParams->build());
+        if ($ret && $ret->result) {
+            $result = json_decode($ret->result, true);
+        } else {
+            return false;
+        }
+        if ($result['status'] == 'OK') {
+            return $result['result'];
+        }else{
+            throw new BadRequestHttpException($result['errors'][0]['message'],$result['errors'][0]['code']);
+        }
+        return false;
+    }
+
+    /**
+     * 获取搜索参数类
+     * @return null
+     * @author: SWZ
+     * @time: 9:32
+     * @date: 2018/11/13
+     * @describe:
+     */
+    public function getSearchParams(){
+        if ($this->_searchParams === null) {
+            // 实例化一个搜索参数类
+            $params = new SearchParamsBuilder();
+            $params->setStart($this->searchStart);
+            $params->setHits($this->hit);
+            $params->setAppName($this->appName);
+            $params->setQuery($this->searchIndex.":".$this->keyWords);
+            $params->setFormat($this->format);
+            //添加排序字段
+            if ($this->sorts){
+                if (is_array($this->sorts) && !$this->sorts['field']){
+                    foreach ($this->sorts as $sort){
+                        $params->addSort($sort['field'], $sort['order'] ?: SearchParamsBuilder::SORT_INCREASE);
+                    }
+                }else{
+                    $params->addSort($this->sorts['field'], $this->sorts['order'] ?: SearchParamsBuilder::SORT_INCREASE);
+                }
+            }
+            $this->_searchParams = $params;
+        }
+        return $this->_searchParams;
     }
 }
---
+++ src/OpenSearchConfig.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IT07
+ * Date: 2018/11/13
+ * Time: 9:02
+ */
+namespace swz\opensearch;
+
+use OpenSearch\Client\OpenSearchClient;
+use OpenSearch\Client\SearchClient;
+use OpenSearch\Util\SearchParamsBuilder;
+use yii\base\Component;
+
+header("Content-Type:text/html;charset=utf-8");
+require_once("OpenSearch/Autoloader/Autoloader.php");
+
+/**
+ * Class OpenSearch
+ * @package swz\opensearch
+ * @property OpenSearchClient client
+ */
+class OpenSearchConfig extends Component
+{
+    public $accessKeyId;
+    public $secret;
+    public $endPoint;
+    public $appName;
+    public $suggestName;
+    public $options;
+
+    public $_client;
+
+    /**
+     * @author: SWZ
+     * @time: 17:10
+     * @date: 2018/11/10
+     * @describe:
+     */
+    public function init()
+    {
+        parent::init();
+        if (!$this->accessKeyId) {
+            throw new \Exception('accessKeyId is empty');
+        }
+        if (!$this->secret) {
+            throw new \Exception('secret is empty');
+        }
+
+        if (!$this->endPoint) {
+            throw new \Exception('endPoint is empty');
+        }
+    }
+
+    /**
+     * 获取OpenSearchClient实例
+     * @return OpenSearchClient
+     * @author: SWZ
+     * @time: 17:09
+     * @date: 2018/11/10
+     * @describe:
+     */
+    public function getClient()
+    {
+        if ($this->_client === null) {
+            $this->_client = new OpenSearchClient($this->accessKeyId, $this->secret, $this->endPoint, $this->options);
+        }
+        return $this->_client;
+    }
+
+    /**
+     * 搜索
+     * @param array $params
+     * @return mixed
+     * @author: SWZ
+     * @time: 17:09
+     * @date: 2018/11/10
+     * @describe:
+     */
+    public function search($data = [])
+    {
+        $searchClient = new SearchClient($this->client);
+        // 实例化一个搜索参数类
+        $params = new SearchParamsBuilder();
+        //设置config子句的start值
+        $params->setStart($data['start'] ?: 0);
+        //设置config子句的hit值
+        $params->setHits($data['hit'] ?: 20);
+        // 指定一个应用用于搜索
+        if ($data['appName']){
+            $params->setAppName($data['appName']);
+        }else{
+            throw new \Exception('appName is empty');
+        }
+        // 指定搜索关键词
+        $params->setQuery(($data['search_index'] ?: 'default').":".$data['key_words']);
+        // 指定返回的搜索结果的格式为json
+        $params->setFormat("fulljson");
+        //添加排序字段
+        if ($data['sorts']){
+            if (is_array($data['sorts'])){
+                foreach ($data['sorts'] as $sort){
+                    $params->addSort($sort['field'], $sort['order'] ?: SearchParamsBuilder::SORT_INCREASE);
+                }
+            }else{
+                $params->addSort($data['sorts']['field'], $data['sorts']['order'] ?: SearchParamsBuilder::SORT_INCREASE);
+            }
+        }
+        // 执行搜索，获取搜索结果
+        $ret = $searchClient->execute($params->build());
+        if ($ret && $ret->result) {
+            $result = json_decode($ret->result, true);
+        } else {
+            return false;
+        }
+        if ($result['status'] == 'OK') {
+            return $result['result'];
+        }
+        return false;
+    }
+}
